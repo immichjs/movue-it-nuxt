3e3accec742286777c7df1ad10c27f63ce9c82e9
Michel França <fr.mich17@gmail.com>
2021-04-04 20:17:46 -0300

    Componente de Countdown concluído

---
+++ components/atoms/CountdownDigits.vue
@@ -0,0 +1,23 @@
+<template>
+    <p class="flex w-1/2 gap-x-1">
+        <span v-for="(digit, index) in splittedDigits" :key="index" class="flex items-center justify-center w-1/2 py-3 bg-white rounded-md">
+            {{ digit }}
+        </span>
+    </p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { splitValue } from '~/utils';
+
+export default Vue.extend({
+    props: {
+        digits: { type: Number, required: true },
+    },
+    computed: {
+        splittedDigits (): string[] {
+            return splitValue(this.digits);
+        },
+    }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,16 @@
 		<div class="flex flex-col w-full lg:w-1/2">
 			<Profile />
 			<CompletedChallenges />
+      <Countdown @completed="getNewChallenge" />
+      <button v-if="hasCountdownCompleted" disabled class="button completed">
+        Ciclo Completado
+      </button>
+      <button v-else-if="isCountdownActive" class="button abandon" @click="setCountdownState(false)">
+        Abandonar Ciclo
+      </button>
+      <button v-else class="button start" @click="setCountdownState(true)">
+        Começar um ciclo
+      </button>
     </div>
   </section>
 </template>
@@ -10,8 +20,17 @@
 <script lang="ts">
 import Vue from 'vue';
 
+import { mapState, mapMutations } from 'vuex';
+import { Mutations as CountdownMT } from '~/store/Countdown/types'
+
 import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue';
 import Profile from '~/components/molecules/Profile.vue';
+import Countdown from '~/components/molecules/Countdown.vue';
+
+import {
+  playAudio,
+  sendNotification,
+} from '~/utils';
 
 interface Head {
   title: string,
@@ -26,6 +45,39 @@ export default Vue.extend({
   components: {
     CompletedChallenges,
     Profile,
+    Countdown,
+  },
+  mounted() {
+    if ('Notification' in window) {
+      Notification.requestPermission();
+    }
+  },
+  computed: {
+    ...mapState('Countdown', {
+      hasCountdownCompleted: 'hasCompleted',
+      isCountdownActive: 'isActive',
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      setCountdownHasCompleted: `Countdown/${CountdownMT.SET_HAS_COMPLETED}`,
+      setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`
+    }),
+    setCountdownState (flag: boolean) {
+      this.setCountdownHasCompleted(false);
+      this.setCountdownIsActive(flag);
+    },
+    getNewChallenge () {
+      this.setCountdownHasCompleted(true);
+      
+      if (Notification?.permission === 'granted') {
+        playAudio('/notification.mp3');
+        sendNotification('Novo Desafio', {
+          body: 'Um novo desafio começou, vá completa-lo!',
+          icon: '/favicon.png'
+        })
+      }
+    }
   }
 });
 </script>
